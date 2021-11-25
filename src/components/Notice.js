187a78cf9a73593379d5364724c75d2793ece443
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-11-26 03:57:20 +0530

    Notice section is created

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Notice = () => {
     return (
-        <div className="container my-3">
+        <div className="container  notice-box">
+            <h1 className="text-center my-3 notice-head">NOTICE</h1>
+            <div className="notice-item my-3">
+            <h3 className="text-center  notice-items"><li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, accusantium.</li></h3> 
+            <hr />
+            <h3 className="text-center  notice-items"><li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, accusantium.</li></h3>  <hr />
+            <h3 className="text-center notice-items"><li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, accusantium.</li></h3>  <hr />
+            <h3 className="text-center notice-items"><li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, accusantium.</li></h3>  <hr />
+            <h3 className="text-center notice-items"><li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, accusantium.</li></h3>  <hr />
+            <h3 className="text-center notice-items"><li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, accusantium.</li></h3>  <hr />
+           
+            </div>
+
+
           
         </div>
     )
