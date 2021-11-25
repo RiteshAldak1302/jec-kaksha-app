1585fa2ee42515832fe25f484ce4f99d40c975a1
Ritesh-Aldak <riteshaldak13022000@gmail.com>
2021-11-26 05:13:30 +0530

    services section is updated

--- src/components/College.js
+++ src/components/College.js
@@ -3,12 +3,13 @@ import React from 'react'
 const College = () => {
     return (
         <div>
-            <div className="container-fluid" style={{"backgroundColor":"#151320" ,"margin":"0px auto"}}>
+            <div className="container-fluid home-box" style={{"backgroundColor":"#151320" ,"margin":"0px auto"}}>
                 <div className="heading d-flex justify-content-center">
             <h1 className="text1 text-center my-3" style={{"fontSize":"58.6px" , "color":"white"}} >Welcome to <div className="center"> <span className="text-head heading-shadow2" style={{"color":"black"}}>JEC</span> <span className="text-head heading-shadow2" style={{"color":"black"}} >KAKSHA</span></div> </h1>
             </div>
             <h2 className="text-center my-3" style={{"color":"white"}}>Please Select Your Year !</h2>
-            <div className="button-sec d-flex justify-content-center" style={{"height":"132px" }}>
+            
+            <div className="button-sec d-flex justify-content-center container" style={{"height":"132px" }}>
             <button class="glow-on-hover mx-3 my-3"  type="button"> <a href="/" style={{"textDecoration":"none" , "color":"white"}}>First Year</a></button>
             <button class="glow-on-hover mx-3 my-3" type="button"><a href="/" style={{"textDecoration":"none" , "color":"white"}}>Second Year</a></button>
             <button class="glow-on-hover mx-3 my-3" type="button"><a href="/" style={{"textDecoration":"none" , "color":"white"}}>Third Year</a></button>
--- src/components/Services.js
+++ src/components/Services.js
@@ -2,8 +2,36 @@ import React from 'react'
 
 const Services = () => {
     return (
-        <div>
-            <h1 className="notice-head">Services</h1>
+        <div className="container services">
+            <h1 className="Services-head">Services</h1>
+            <div class="row1-container">
+    <div class="box box-down cyan">
+    <h3>PREVIOUS YEAR PAPER | LATEST NOTICE</h3>
+      <p>For providing a bett Lorem ipsum dolor sit amet. tuned</p>
+      <img src="https://assets.codepen.io/2301174/icon-calculator.svg" alt=""/>
+      
+    </div>
+
+    <div class="box red">
+      <h3>SYLLABUS  </h3>
+      <p>Scans our talent network to create the optimal team for your project</p>
+      <img src="https://assets.codepen.io/2301174/icon-team-builder.svg" alt=""/>
+    </div>
+
+    <div class="box box-down blue">
+    <h3>NEW PROJECT</h3>
+      <p>For providing a better experience to our users we are developing our Android application, the application will have a lot of awesome features so stay tuned</p>
+      <img src="https://assets.codepen.io/2301174/icon-karma.svg" alt=""/>
+    </div>
+  </div>
+  <div class="row2-container">
+    <div class="box orange">
+    <h3>COMPLETELY FREE</h3>
+      <p>Yup, everything is free and all the files are uploaded to our super-fast servers</p>
+      <img src="https://assets.codepen.io/2301174/icon-supervisor.svg" alt=""/>
+      
+    </div>
+  </div>
         </div>
     )
 }
